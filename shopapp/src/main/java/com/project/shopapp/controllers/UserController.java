6de975af18ba9898f7c90cd9d78b53833bb5cfd2
Nthoang2002 <123498505+Nthoang2002@users.noreply.github.com>
2024-09-26 20:40:34 +0700

    UserController và UserRepository, chưa dùng Spring Security

--- shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
+++ shopapp/src/main/java/com/project/shopapp/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.project.shopapp.controllers;
 
 import com.project.shopapp.dtos.*;
+import com.project.shopapp.services.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -14,7 +16,10 @@ import java.util.List;
 
 @RestController
 @RequestMapping("${api.prefix}/users")
+@RequiredArgsConstructor
+
 public class UserController {
+    private final UserService userService;
 
     @PostMapping("/register")
     public ResponseEntity<?> createUser(
@@ -32,6 +37,7 @@ public class UserController {
             if(!userDTO.getPassword().equals(userDTO.getRetypePassword())){
                 return ResponseEntity.badRequest().body("Password does not match");
             }
+            userService.createUser(userDTO);
             return ResponseEntity.ok("Register successfully");
         }catch (Exception e) {
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -39,7 +45,11 @@ public class UserController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@Valid @RequestBody UserLoginDTO userLoginDTO){
+    public ResponseEntity<?> login(
+            @Valid @RequestBody UserLoginDTO userLoginDTO){
+        // Kiểm tra thông tin đăng nhập và sinh token
+        String token = userService.login(userLoginDTO.getPhoneNumber(), userLoginDTO.getPassword());
+        // Trả về token trong response
         return ResponseEntity.ok("some token");
     }
 }
